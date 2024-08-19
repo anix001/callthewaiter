897a670de5c04449468b279ad0b57d41d8745029
anix001 <chaudharyanish305@gmail.com>
2024-08-20 00:09:22 +0545

    Improvement: Token undefined check condtiton

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -13,8 +13,8 @@ export const authenticateToken = (
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (token === null) return res.sendStatus(401);
-  if (token !== undefined) {
+  if (token === null || token === undefined) return res.sendStatus(401);
+
     jwt.verify(
       token,
       process.env.ACCESS_TOKEN_SECRET as string,
@@ -24,5 +24,4 @@ export const authenticateToken = (
         next();
       }
     );
-  }
 };
